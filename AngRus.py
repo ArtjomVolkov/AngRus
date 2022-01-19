06cd3049e8a570fafd27e249c057d5a6649154fe
opilane <opilane@HP-CZC6297B8Z>
2022-01-19 14:13:16 +0200

    SS

--- AngRus.py
+++ AngRus.py
@@ -1,27 +1,46 @@
 from module1 import*
 rus=[]
 ang=[]
-ang=ang_failist("Ang.txt",ang)
-rus=rus_failist("Rus.txt",rus)
+ang=lug("Ang.txt",ang)
+rus=lug("Rus.txt",rus)
 print(ang)
 print(rus)
-#russlov=str(input("Добавьте русское слово ->"))
-#rus=rida_salvestamine("Rus.txt",russlov)
-#angslov=str(input("Add an English word ->"))
-#ang=rida_salvestamine("Ang.txt",angslov)
-#print(rus)
-#print(ang)
-print("Добро пожаловать в Русско-Английский словарь \nWelcome to the Russian-English Dictionary")
+print("Добро пожаловать в Русско-Эстониский словарь \nTere tulemast vene-eesti sõnastikku!")
 while True:
-	try:
-		ch=int(input("Вы хотите с Английского на Русский (1) или с Русского на Английский (2) \nDo you want from English to Russian (1) or from Russian to English (2) -> "))
-	except ValueError:
-		print("Выберите 1 или 2 \nChoose 1 or 2")
-	if ch==1:
-		print()
-		print("Словарь с Английского на Русский \nDictionary from English to Russian")
-		print()
-	if ch==2:
-		print()
-		print("Словарь с Русского на Английский \nDictionary from Russian to English")
-		print()
+	while 1:
+		try:
+			menu=input("\nRääkida - R \nKõik sõnad - S \nTõlgida - T \nUus sõna - U \nViga - V \nKontroll - K \nLõpp - L \n")
+		except ValueError:
+			print()
+			print("Valige R,S,T,U,V,K,L")
+			print()
+		if menu.upper()=="U":
+			rus=newWord("Rus.txt",input("Новое слово -> "))
+			ang=newWord("Ang.txt",input("Uus sõna -> "))
+		elif menu.upper()=="S":
+			print(rus)
+			print(ang)
+		elif menu.upper()=="T":
+			tolkimine(rus,ang)
+		elif menu.upper()=="U":
+			print()
+		elif menu.upper()=="V":
+			viga(rus,"Rus.txt",ang,"Ang.txt")
+			#keel=input("Mis keel?")
+			#if keel=="rus":
+			#	rus=correction(input("Слово -> "),rus)
+			#	failisse(rus,"Rus.txt")
+			#else:
+			#	ang=correction(input("Sõna -> "),ang)
+			#	failisse(ang,"Ang.txt")
+		elif menu.upper()=="K":
+			print()
+		elif menu.upper()=="L":
+			print("Удачи! \nHead aega!")
+			exit(0)
+		elif menu.upper()=="R":
+			keel=input("Mis keeles ütelda? ->")#ru et en fi
+			sona=input("Sõna -> ")
+			heli(sona,keel)
+		else:
+			break
--- module1.py
+++ module1.py
@@ -1,22 +1,97 @@
-def rus_failist(f:str,u:list):
-	"""
-	"""
-	fail=open(f,'r',encoding="utf-8-sig")
-	for rida in fail:
-		u.append(rida.strip())
-	fail.close()
-	return u
 
-def ang_failist(a:str,p:list):
+def lug(f:str,l:list):
+    fail=open(f,"r",encoding="utf-8-sig")
+    mas=[] 
+    for rida in fail:
+        l.append(rida.strip())
+    fail.close()
+    return l
+
+def failisse_salvestamine(f:str,l:list):
+	"""Loetelu salvaestame failisse
 	"""
-	"""
-	fail=open(a,"r")#чтение файла
-	for rida in fail:
-		p.append(rida.strip())
+	fail=open(f,"w")#чтение файла
+	for el in l:
+		fail.write(el+"\n")
 	fail.close()
-	return p
 
 def rida_salvestamine(f:str,rida:str):
+	"""
+	"""
 	fail=open(f,"a")#режим до записи
 	fail.write(rida+"\n")
 	fail.close()
+
+def newWord(f:str, rida:str)->list:
+    l=[]
+    with open(f,"a",encoding="utf-8-sig") as fail:
+        fail.write(rida+"\n")
+    l=lug(f,l)
+    return l
+
+def uus_sona(f:str,rida:str)->list:
+	"""Lisame uus sõna failisse ja listisse
+	:param str file: Faili nimetus
+	:param str x: lisatav sõna
+	"""
+	l=[]
+	with open(f,"a",encoding="utf-8-sig") as fail:
+		fail.write(rida+"\n")
+	l=failist_lugemine(f,1)
+	return l
+
+def tolkimine(l1:list,l2:list):
+	"""Tõlkimine sõna
+	:param list l1:
+	:param list l2:
+	"""
+	sona=input("Mida tõlkida? ")
+	if sona in l1:
+		tolk=l2[l1.index(sona)]
+		print(sona+"="+tolk)
+	elif sona in l2:
+		tolk=l1[l2.index(sona)]
+		print(sona+"="+tolk)
+	else:
+		print("Sõna puudub sõnastikus!")
+
+def correction(sona:str,l:list):
+	for i in range(len(l)):
+		if l[i]==sona:
+			uus_sona=sona.replace(sona,input("Uus sõna -> "))
+			l.insert(i,uus_sona)
+			l.remove(sona)
+	return l
+
+def viga(l1:list,f1:str,l2:list,f2:str):
+	"""
+	"""
+	sona=input("Sõna veaga? -> ")
+	if sona not in l1 and sona not in l2:
+		print("Sõna puudub!")
+	else:
+		if sona in l1:
+			tolk=l2[l1.index(sona)]
+			l1.remove(sona)
+			l2.remove(tolk)
+		elif sona in l2:
+			tolk=l1[l2.index(sona)]
+			l2.remove(sona)
+			l1.remove(tolk)
+		uus_sona(l1,input("Введите новое слово -> "))
+		uus_sona(l2,input("Sisesta uus sõna -> "))
+		failfailisse_salvestamine(f1,l1)
+		failisse_salvestamine(f2,l2)
+
+def failisse(mas:list,file:str):
+	f=open(file,"w",encoding="utf-8-sig")
+	for sona in mas:
+		f.write(sona+"\n")
+		f.close()
+
+import os
+from gtts import gTTS
+def heli(text:str,keel:str):
+	obj=gTTS(text=text,lang=keel,slow=False).save("heli.mp3")
+	os.system("heli.mp3")
+	playsound("heli.mp3")
